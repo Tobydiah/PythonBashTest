79801fdf2753b21a9f64f42f3b2ad459d6f203d8
Yoonki Park <Yoonki_Park@ultimatesoftware.com>
2021-07-25 15:46:59 -0400

    My first modification. Just a message.

--- TeamTool.py
+++ TeamTool.py
@@ -1,4 +1,8 @@
 This is the team automation tool.
 Not really. This is just a test file that 
 originated in this remote repo but as if it was already 
-a work in progress I should start adding onto. 
+a work in progress I should start adding onto.
+
+Locally Modified at 2021/07/25
+3:45PM ET
+Yoonki Park
