e8046a90aa2722817c9fb1fd4c968d52c331205b
Tobydiah <yoonkipark01@gmail.com>
2021-07-25 15:40:23 -0400

    Create TeamTool.py

---
+++ TeamTool.py
@@ -0,0 +1,4 @@
+This is the team automation tool.
+Not really. This is just a test file that 
+originated in this remote repo but as if it was already 
+a work in progress I should start adding onto. 
